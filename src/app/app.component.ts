bb0b71a387e67dacdfbe6428926352b1b519620e
Anubhav7050 <anubhavkumar7050@gmail.com>
2025-04-13 11:26:01 +0530

    functinalities added for to do list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,35 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'to-doList';
+  editedText:any;
+  buttonText: string="ADD";
+  isEditButtonShow:boolean= false;
+  input: string="";
+  taskArray: any[]=[];
+  addItem(){
+    if (this.buttonText==="ADD") {
+      this.taskArray.unshift(this.input);
+    }else{
+      this.taskArray[this.taskArray.indexOf(this.editedText)]=this.input;
+    }
+    this.input="";
+  }
+  deleteItem(i: any){
+    this.taskArray.splice(i,1);
+  }
+  editItem(item:string){
+    this.input=item;
+    this.buttonText="Edit";
+    this.editedText=item;
+  } 
 }
